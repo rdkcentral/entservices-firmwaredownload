30458ea58668ff43a4692df9611f27372499cf66
balav08 <54432605+balav08@users.noreply.github.com>
2026-06-09 19:54:03 +0530

    Update build_dependencies.sh

--- build_dependencies.sh
+++ build_dependencies.sh
@@ -27,7 +27,7 @@ cd ..
 git clone --branch develop https://github.com/rdkcentral/entservices-helpers.git
 cd "$GITHUB_WORKSPACE"
 
-git clone --branch 1.0.14 https://$GITHUB_TOKEN@github.com/rdkcentral/entservices-testframework.git
+git clone --branch 1.0.14 https://github.com/rdkcentral/entservices-testframework.git
 
 ############################
 # Build Thunder-Tools
